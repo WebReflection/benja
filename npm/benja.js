de59632cd383c2b8efb29caf686c5e47b46e1d1e
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2016-06-19 14:03:03 +0100

    updated module

--- npm/benja.js
+++ npm/benja.js
@@ -1,3 +1,28 @@
-this.require = function (module) {
-  return require('/home/benja/app/node_modules/' + module);
-};
+var
+  fs = require('fs'),
+  globalPaths = require('module').globalPaths
+;
+
+// fix Electron paths once loaded in B.E.N.J.A.
+(function verify(PATH) {
+  // if PATH is defined and is not already considered
+  if (PATH && globalPaths.indexOf(PATH) < 0) {
+    // try to see if it exists
+    try {
+      fs.statSync(PATH);
+      // and in such case add it to the list
+      globalPaths.push(PATH);
+    } catch(ignore) {}
+  }
+  // returns this IIEF for chained invokes
+  return verify;
+}(
+  // should be the same as '/home/benja/app/node_modules'
+  process.cwd() + '/node_modules'
+)(
+  // should be defined by Benja OS
+  process.env.NODE_PATH
+)(
+  // to be sure it's available
+  '/home/benja/app/node_modules'
+));
