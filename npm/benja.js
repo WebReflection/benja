7bb34ebc9036859e78b020e48b03d944326bd198
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2016-06-19 14:11:07 +0100

    better

--- npm/benja.js
+++ npm/benja.js
@@ -4,25 +4,24 @@ var
 ;
 
 // fix Electron paths once loaded in B.E.N.J.A.
-(function verify(PATH) {
-  // if PATH is defined and is not already considered
-  if (PATH && globalPaths.indexOf(PATH) < 0) {
-    // try to see if it exists
-    try {
-      fs.statSync(PATH);
-      // and in such case add it to the list
-      globalPaths.push(PATH);
-    } catch(ignore) {}
-  }
-  // returns this IIEF for chained invokes
-  return verify;
-}(
+[
   // should be the same as '/home/benja/app/node_modules'
-  process.cwd() + '/node_modules'
-)(
+  process.cwd() + '/node_modules',
+
   // should be defined by Benja OS
-  process.env.NODE_PATH
-)(
+  process.env.NODE_PATH,
+
   // to be sure it's available
   '/home/benja/app/node_modules'
-));
+
+].join(':').split(':').forEach(function (path) {
+  // if path is defined and not already considered
+  if (path && globalPaths.indexOf(path) < 0) {
+    // verify it exists
+    try {
+      fs.statSync(path);
+      // and add it to the list
+      globalPaths.push(path);
+    } catch(ignore) {}
+  }
+});
